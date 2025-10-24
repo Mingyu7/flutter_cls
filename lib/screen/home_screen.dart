58d76124cee8a11824c9f54fd804069ae649aaa1
mingyu <vkans7526@naver.com>
2025-10-24 11:42:51 +0900

    up

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:untitled1/component/main_calendar.dart';
 
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime selectedDate = DateTime.utc( //
+  DateTime selectedDate = DateTime.utc(
     DateTime.now().year,
     DateTime.now().month,
     DateTime.now().day,
   );
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    // 날짜 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea( // 시스템 UI 피해서 UI -
-            child: Column( // 달력과 리스트를 세로로
-                children: [
-                  // 미리 작업해둔 달력 위젯 보여주기
-                  MainCalendar(),
-                ]
-            )
-        )
+      body: SafeArea( // 시스템 UI 피해서 UI 표시
+        child: Column( // 달력과 리스트를 세로로 배치
+          children: [
+            // 미리 작업해둔 달력 위젯 보여주기
+            MainCalendar(
+              selectedDate: selectedDate, // 선택 날짜 전달
+              onDaySelected: onDaySelected, // 날짜 선택 시 실행할 함수 전달
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
