46f6e2979df77d133e1fc1ca36b2f808c985ecd5
mingyu <vkans7526@naver.com>
2025-10-24 11:13:32 +0900

    first

---
+++ lib/component/main_calendar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MainCalendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime(1800, 1, 1), // 1 첫째 날
+      lastDay: DateTime(3000, 1, 1), // 2 마지막 날
+      focusedDay: DateTime.now(),    // 3 화면에 보여지는 날
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/component/main_calendar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea( // 시스템 UI 피해서 UI -
+            child: Column( // 달력과 리스트를 세로로
+                children: [
+                  // 미리 작업해둔 달력 위젯 보여주기
+                  MainCalendar(),
+                ]
+            )
+        )
+    );
+  }
+}
